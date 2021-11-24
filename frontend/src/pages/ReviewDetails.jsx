c6a9f8a12b976353da3308be609b41f76114ed8f
skmm6 <egor.qaq@gmail.com>
2021-11-24 16:26:03 +0300

    24.11.21

--- frontend/src/pages/ReviewDetails.jsx
+++ frontend/src/pages/ReviewDetails.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+
 
 export default function ReviewDetails() {
+    const { id } = useParams()
+    const { loading, error, data } = useFetch('http://localhost:1337/reviews/' + id)
+ 
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+console.log(data);
+     
     return (
-        <div>
-            rewiewdetails
+        <div className="review-card">
+          <div className="rating">{data.rating}</div>
+          <h2>{data.title}</h2>
+          
+          <small>console list</small>
+
+          <p>{data.body}...</p>
         </div>
     )
 }
